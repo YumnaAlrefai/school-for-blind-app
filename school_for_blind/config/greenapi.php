b4d0f0e7d6b65489c99a776b5bea6079b56c8d30
Yumna Al Refai <yumnaalrefai@gmail.com>
2026-04-29 11:29:56 +0300

    Delete school_for_blind directory

--- school_for_blind/config/greenapi.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'url'      => env('GREEN_API_URL', 'https://api.green-api.com'),
-    'instance' => env('GREEN_API_INSTANCE_ID'),
-    'token'    => env('GREEN_API_TOKEN'),
-];
