0b52839c78992d0589c30fd6588795b581a72ba7
maheralnouri244-lab <maheralnouri244@gmail.com>
2026-06-16 15:51:15 +0300

    LOGIN

--- Backend/school_for_blind/app/Http/Controllers/MagicLoginController.php
+++ Backend/school_for_blind/app/Http/Controllers/MagicLoginController.php
@@ -14,6 +14,22 @@ use App\Traits\UploadFileTrait;
 class MagicLoginController extends Controller
 {
     use UploadFileTrait;
+
+    public function showView(Request $request, $id)
+    {
+        $student = Student::findOrFail($id);
+        $postUrl = $this->generateMagicSignedRoute('student.magic.generate', 15, ['id' => $student->id]);
+        $postUrl = URL::temporarySignedRoute(
+            'student.magic.generate',
+            now()->addMinutes(15),
+            ['id' => $student->id]
+        );
+
+        return view('magic-login-page', [
+            'student' => $student,
+            'postUrl' => $postUrl
+        ]);
+    }
     public function generateToken(Request $request, $id)
     {
         $student = Student::findOrFail($id);
@@ -49,21 +65,21 @@ class MagicLoginController extends Controller
         if (!$student) {
             return response()->json(['message' => 'الطالب غير موجود.'], 404);
         }
-      $documentUrl = $this->getSignedDocumentUrl($student->DocumentaryEvidence);
+        $documentUrl = $this->getSignedDocumentUrl($student->DocumentaryEvidence);
         $realToken = $student->createToken('mobile-app')->plainTextToken;
         return response()->json([
             'access_token' => $realToken,
             'student' => [
-            'id'                   => $student->id,
-            'fullname'             => $student->fullname,
-            'fathersname'          => $student->fathersname,
-            'phone'                => $student->phone,
-            'parent_phone'         => $student->parent_phone,
-            'level'                => $student->level,
-            'status'               => $student->status,
-            'DocumentaryEvidence'  => $documentUrl,
-        ]
-    ], 200, [], JSON_UNESCAPED_UNICODE);
+                'id' => $student->id,
+                'fullname' => $student->fullname,
+                'fathersname' => $student->fathersname,
+                'phone' => $student->phone,
+                'parent_phone' => $student->parent_phone,
+                'level' => $student->level,
+                'status' => $student->status,
+                'DocumentaryEvidence' => $documentUrl,
+            ]
+        ], 200, [], JSON_UNESCAPED_UNICODE);
 
     }
 }
